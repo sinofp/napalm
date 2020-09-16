147eab6ea229aab777897b495b26c9d073d5c7ca
sinofp <sinofp@tuta.io>
2020-09-16 22:30:02 +0800

    Init pipelined version

---
+++ napalm.srcs/sources_1/new/pc.v
@@ -0,0 +1,19 @@
+`timescale 1ns / 1ps
+
+
+module pc(
+           input             clk,
+           input             rst,
+           input [31:0]      pc_next,
+           output reg [31:0] pc_now
+       );
+    
+always @ (posedge clk) begin
+    if (rst) begin
+        pc_now <= 32'b0;
+    end
+    else begin
+        pc_now <= pc_next;
+    end
+end
+endmodule
