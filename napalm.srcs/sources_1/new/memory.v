fd02e049ff825114e00882fdb077c6ce46112674
sinofp <sinofp@tuta.io>
2020-09-19 17:12:29 +0800

    Add reg to mem

--- napalm.srcs/sources_1/new/memory.v
+++ napalm.srcs/sources_1/new/memory.v
@@ -3,16 +3,41 @@
 
 module memory (/*AUTOARG*/
    // Outputs
-   data_rd, res_w, reg_wa_w,
+   data_rd, reg_wa,
    // Inputs
-   res_m, data_wd_m, reg_wa_m
+   clk, rst, res_m, _wd, _memWriteEn, reg_wa_m, _res
    ) ;
-   input  [31:0] res_m, data_wd_m;
+   input clk, rst;
+   input  [31:0] res_m, _wd;
+   input _memWriteEn;
    input [4:0]   reg_wa_m;
-   output [31:0] data_rd, res_w;
-   output [4:0]       reg_wa_w;
+   input [31:0] _res;
+   output reg [31:0] data_rd;
+   output reg [4:0]       reg_wa;
 
-   assign reg_wa_w = reg_wa_m;
-   assign res_w = res_m;
+   reg [31:0] wd, res;
+   reg memWriteEn;
+
+   always @(posedge clk) begin
+      if (rst) begin
+         res <= 32'b0;
+         reg_wa <= 5'b0;
+         wd <= 32'b0;
+         memWriteEn <= 1'b0;
+      end else begin
+         res <= _res;
+         reg_wa <= reg_wa_m;
+         wd <= _wd;
+         memWriteEn <= _memWriteEn;
+      end
+   end
+
+   data_mem DATA_MEM(
+    .clk(clk),
+    .addr(res),
+    .wd(wd),
+    .we(memWriteEn),
+    .rd(data_rd)
+   );
 
 endmodule // memory
