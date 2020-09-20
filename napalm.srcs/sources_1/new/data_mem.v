c41bcd9ab8fe580f1c2dcbfa79c499b8a16db417
sinofp <sinofp@tuta.io>
2020-09-20 18:34:52 +0800

    Add lb support

--- napalm.srcs/sources_1/new/data_mem.v
+++ napalm.srcs/sources_1/new/data_mem.v
@@ -1,23 +1,26 @@
 `timescale 1ns / 1ps
+`include "def.vh"
 
 module data_mem (
     input         clk,
-    input  [31:0] addr,  // 写入/读取的地址
-    input  [31:0] wd,  // write_data
-    input         we,  // write_enable
+    input  [31:0] _addr,  // 写入/读取的地址
+    input  [31:0] _wd,  // write_data
+    input         _we,  // write_enable
+    input  [5:0]  _opcode,
     output [31:0] rd  // read_data
 );
 
   reg [7:0] mem[255:0];
 
-  assign rd = {mem[addr+3], mem[addr+2], mem[addr+1], mem[addr]};
+  assign rd = (_opcode == `LB_OP)? {{24{mem[_addr][7]}}, mem[_addr]}: // load 8 位，符号扩展到 32 位
+                                   {mem[_addr+3], mem[_addr+2], mem[_addr+1], mem[_addr]};
 
   always @(posedge clk) begin
-    if (we) begin
-      mem[addr+3] <= wd[31:24];
-      mem[addr+2] <= wd[23:16];
-      mem[addr+1] <= wd[15:8];
-      mem[addr] <= wd[7:0];
+    if (_we) begin
+      mem[_addr+3] <= _wd[31:24];
+      mem[_addr+2] <= _wd[23:16];
+      mem[_addr+1] <= _wd[15:8];
+      mem[_addr]   <= _wd[7:0];
     end
   end
 endmodule  // data_mem
