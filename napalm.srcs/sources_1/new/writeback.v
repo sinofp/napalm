78f232e982457a33970704fc48ee1a9a59f2a048
YUPP12138 <70902032+YUPP12138@users.noreply.github.com>
2020-09-20 18:16:43 +0800

    Update writeback.v

--- napalm.srcs/sources_1/new/writeback.v
+++ napalm.srcs/sources_1/new/writeback.v
@@ -6,10 +6,10 @@ module writeback (
     input             clk,
     input             rst,
     input      [31:0] _mem_data,  // 从内存中读出的数据
-    input      [31:0] _alu_alu_res,  // 从alu得到的结果
+    input      [31:0] _alu_res,  // 从alu得到的结果
     input      [31:0] _imm_ext,  // lui的立即数
     input             _jump_link,  // 跳转的link addr
-    input      [ 2:0] _reg_write_data_nux,  // cu的srcReg，用上面哪个数据写回
+    input      [ 2:0] _reg_write_data_mux,  // cu的srcReg，用上面哪个数据写回
     input      [ 4:0] _reg_write_addr,  // 写到哪个寄存器
     input             _reg_we,  // write enable
     output reg        reg_we,
@@ -18,7 +18,7 @@ module writeback (
 );
 
   reg [31:0] mem_data, alu_res, imm_ext, jump_link;
-  reg [2:0] reg_write_data_nux;
+  reg [2:0] reg_write_data_mux;
 
   always @(posedge clk) begin
     if (rst) begin
@@ -26,19 +26,19 @@ module writeback (
     end else begin
       reg_we <= _reg_we;
       mem_data <= _mem_data;
-      alu_res <= _alu_alu_res;
+      alu_res <= _alu_res;
       imm_ext <= _imm_ext;
       jump_link <= _jump_link;
-      reg_write_data_nux <= _reg_write_data_nux;
+      reg_write_data_mux <= _reg_write_data_mux;
       reg_write_addr <= _reg_write_addr;
     end
   end
 
-  assign reg_write_data = (reg_write_data_nux == `SRC_WRITE_REG_IMM)? imm_ext:
-                    (reg_write_data_nux == `SRC_WRITE_REG_ALU)? alu_res:
-                    (reg_write_data_nux == `SRC_WRITE_REG_MEM)? mem_data:
-                    (reg_write_data_nux == `SRC_WRITE_REG_MEM)? mem_data:
-                    (reg_write_data_nux == `SRC_WRITE_REG_JDST)? jump_link:
-                    32'bx; // 不应该有这种情况——或者说这种情况并不用写回
+  assign reg_write_data = (reg_write_data_mux == `SRC_WRITE_REG_IMM)? imm_ext:
+                    (reg_write_data_mux == `SRC_WRITE_REG_ALU)? alu_res:
+                    (reg_write_data_mux == `SRC_WRITE_REG_MEM)? mem_data:
+                    (reg_write_data_mux == `SRC_WRITE_REG_MEM)? mem_data:
+                    (reg_write_data_mux == `SRC_WRITE_REG_JDST)? jump_link:
+                    32'bx; // 不应该有这种情况--或者说这种情况并不用写回
   //TODO 给decode的register file
 endmodule  // writeback
