1334b66ee50c2912046fc6fb741b3a86fe2e4b1d
sinofp <sinofp@tuta.io>
2020-09-20 10:04:55 +0800

    Add reg to fetch
    还有停机的没写进去

--- napalm.srcs/sources_1/new/fetch.v
+++ napalm.srcs/sources_1/new/fetch.v
@@ -3,19 +3,34 @@
 module fetch (
     input clk,
     input rst,
+    input [31:0] _pc_next,
+    input [31:0] _pc_jump,
+    input _jump,
     output [31:0] pcp4d,
     output [31:0] inst
 );
 
   assign pcp4d = pc_now + 32'h4;
 
-  wire [31:0] pc_now, pc_next;  // TODO pcnext逻辑，当然还有冲刷流水线
+  wire [31:0] pc_now;
+
+  reg [31:0] pc_jump;
+  reg jump;
+
+  always @(posedge clk) begin
+    if (rst) begin
+      jump <= 0;
+    end else begin
+      pc_jump <= _pc_jump;
+      jump <= _jump;
+    end
+  end
 
   pc PC (
       .clk(clk),
       .rst(rst),
       .pc_now(pc_now),
-      .pc_next(pc_next)
+      .pc_next(jump? pc_jump: pcp4d)
   );
 
   inst_mem INST_MEM (
