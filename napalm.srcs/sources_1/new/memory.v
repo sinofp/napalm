c4b1ddc9ec023808e06f70ed541fd848a317176e
YUPP12138 <70902032+YUPP12138@users.noreply.github.com>
2020-09-20 21:13:53 +0800

    Update memory.v

--- napalm.srcs/sources_1/new/memory.v
+++ napalm.srcs/sources_1/new/memory.v
@@ -8,7 +8,17 @@ module memory (
     input _mem_we,  // 要写入么？
     input [31:0] _alu_res,  // alu的运算结果，同时也是访存的地址
     input [4:0] _reg_write_addr,  // 给写回阶段的，写回到哪个寄存器--reg_file.wa
-    output reg [31:0] mem_data,  // 读出的数据
+    input      [31:0] _pcp8,
+	input      [5:0] _op_code,
+	input _reg_we,
+	input      [2:0] _reg_wd_mux,
+	input      [31:0] _imm_ext,
+	output reg_we,
+	output reg [31:0] mem_data,  // 读出的数据
+	output [31:0] alu_res,
+	output [31:0] imm_ext,
+	output [2:0] reg_wd_mux,
+	output [31:0] pcp8,
     output reg [4:0] reg_write_addr  // 给写回的，和_reg_write_addr差一个周期
 );
 
