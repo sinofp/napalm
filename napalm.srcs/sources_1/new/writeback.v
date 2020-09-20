e26a8e1524afaf64ca6d481008ee8527ab0326f3
YUPP12138 <70902032+YUPP12138@users.noreply.github.com>
2020-09-20 20:44:50 +0800

    Update writeback.v

--- napalm.srcs/sources_1/new/writeback.v
+++ napalm.srcs/sources_1/new/writeback.v
@@ -1,17 +1,16 @@
 `timescale 1ns / 1ps
 `include "def.vh"
 
-
 module writeback (
     input             clk,
     input             rst,
     input      [31:0] _mem_data,  // 从内存中读出的数据
     input      [31:0] _alu_res,  // 从alu得到的结果
     input      [31:0] _imm_ext,  // lui的立即数
-    input             _jump_link,  // 跳转的link addr
-    input      [ 2:0] _reg_write_data_mux,  // cu的srcReg，用上面哪个数据写回
+    input      [ 2:0] _reg_wd_mux,  // cu的srcReg，用上面哪个数据写回
     input      [ 4:0] _reg_write_addr,  // 写到哪个寄存器
     input             _reg_we,  // write enable
+	input      [31:0] pcp8,
     output reg        reg_we,
     output reg [ 4:0] reg_write_addr,  // 同上，但慢一个周期
     output     [31:0] reg_write_data
@@ -28,8 +27,7 @@ module writeback (
       mem_data <= _mem_data;
       alu_res <= _alu_res;
       imm_ext <= _imm_ext;
-      jump_link <= _jump_link;
-      reg_write_data_mux <= _reg_write_data_mux;
+      reg_write_data_mux <= _reg_wd_mux;
       reg_write_addr <= _reg_write_addr;
     end
   end
@@ -39,6 +37,6 @@ module writeback (
                     (reg_write_data_mux == `SRC_WRITE_REG_MEM)? mem_data:
                     (reg_write_data_mux == `SRC_WRITE_REG_MEM)? mem_data:
                     (reg_write_data_mux == `SRC_WRITE_REG_JDST)? jump_link:
-                    32'bx; // 不应该有这种情况——或者说这种情况并不用写回
+                    32'bx; // 不应该有这种情况--或者说这种情况并不用写回
   //TODO 给decode的register file
 endmodule  // writeback
