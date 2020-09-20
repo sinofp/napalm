7205a2e0577f1f08fbd92592efc91679773603b1
SingleZombie <singlezombie@163.com>
2020-09-20 10:46:58 +0800

    Update br_unit.v
    Finish br_unit

--- napalm.srcs/sources_1/new/br_unit.v
+++ napalm.srcs/sources_1/new/br_unit.v
@@ -12,18 +12,21 @@ module br_unit (
     output jump  // 跳转不跳
 );
   wire conditionSatisfied;
-  assign condtionSatisfied = ;
+  assign condtionSatisfied = (mode == `BR_OP_GREATER && rd1 > rd2) ||
+  (mode == `BR_OP_GREATER_EQ && rd1 >= rd2) || 
+  (mode == `BR_OP_EQUAL && rd1 == rd2) || 
+  (mode == `BR_OP_NOT_EQUAL && rd1 != rd2) || 
+  (mode == `BR_OP_LESS && rd1 < rd2) || 
+  (mode == `BR_OP_LESS_EQ && rd1 <= rd2) 
+  ;
 
-  assign jump = (mode == `BR_OP_LEN'd0) ? 0 : 
-  (mode == `BR_OP_ ) ? 1 :
+  assign jump = (mode == `BR_OP_DEFAULT) ? 0 : 
+  (mode == `BR_OP_DIRECTJUMP || mode == `BR_OP_REG) ? 1 :
   (conditionSatisfied) ? 1 : 0;
 
   wire [31:0] imm_sl2;
-  assign imm_sl2 = {imm_ext[29 : 0], 2'b00};
-  assign pc_jump = () ? {pcp4[31 : 0], imm_sl2} : 
-  () ? (pcp4 + imm_sl2) : 31'd0;
-
-
-    // beq, j ...
+  assign imm_sl2 = (mode == `BR_OP_REG) ? rd1 : {imm_ext[29 : 0], 2'b00};
+  assign pc_jump = (mode == `BR_OP_DIRECTJUMP || mode == `BR_OP_REG) ? {pcp4[31 : 28], imm_sl2} : 
+  (conditionSatisfied) ? (pcp4 + imm_sl2) : 31'd0;
 endmodule  // br_unit
 
