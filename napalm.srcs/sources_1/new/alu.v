dd565ae0d4ce815ba18ed7328c22b30b83f35b18
YUPP12138 <YUPP12138@users.noreply.github.com>
2020-09-20 07:43:46 +0000

    Format Verilog code

--- napalm.srcs/sources_1/new/alu.v
+++ napalm.srcs/sources_1/new/alu.v
@@ -5,41 +5,36 @@
 module alu (
     input [31:0] _num1,
     input [31:0] _num2,
-	input [4:0] _sa,
-	input [3:0] _alu_op,
+    input [4:0] _sa,
+    input [3:0] _alu_op,
     output [31:0] alu_res,
     output reg overflow
 );
 
 
-reg[32:0] alu_reg;
-//reg[32:0] alu_hi;
-//reg[32:0] alu_lo;
-assign alu_res = alu_reg[31:0];
-//assign hi = alu_hi[31:0];//for DIV, MULT
-//assign lo = alu_lo[31:0];//32 bits
+  reg [32:0] alu_reg;
+  //reg[32:0] alu_hi;
+  //reg[32:0] alu_lo;
+  assign alu_res = alu_reg[31:0];
+  //assign hi = alu_hi[31:0];//for DIV, MULT
+  //assign lo = alu_lo[31:0];//32 bits
 
-always @ (*) begin
+  always @(*) begin
     case (_alu_op)
-        `ALU_OP_PLUS:// +
+      `ALU_OP_PLUS:// +
 		begin
-            alu_reg <= _num1 + _num2;
-			if((_num1 >= 0) && (_num2 >= 0) && (alu_reg < 0))
-			begin
-				overflow <= 1'b1;
-			end
-			else if((_num1 < 0) && (_num2 < 0) && (alu_reg >= 0))
-			begin
-				overflow <= 1'b1;
-			end
-			else
-			begin
-				overflow <= 1'b0;// no overflow
-			end
-		end
-        `ALU_OP_AND://&
+        alu_reg <= _num1 + _num2;
+        if ((_num1 >= 0) && (_num2 >= 0) && (alu_reg < 0)) begin
+          overflow <= 1'b1;
+        end else if ((_num1 < 0) && (_num2 < 0) && (alu_reg >= 0)) begin
+          overflow <= 1'b1;
+        end else begin
+          overflow <= 1'b0;  // no overflow
+        end
+      end
+      `ALU_OP_AND://&
 			alu_reg <= _num1 & _num2;
-/*	    `ALU_OP_DIV:// / %
+      /*	    `ALU_OP_DIV:// / %
 		begin
 			begin
 				alu_hi <= _num1 % _num2;
@@ -120,45 +115,39 @@ always @ (*) begin
 			end
 		end
 */
-        `ALU_OP_OR:// |
+      `ALU_OP_OR:// |
             alu_reg <= _num1 | _num2;
-        `ALU_OP_SLL:// <<
+      `ALU_OP_SLL:// <<
             alu_reg <= _num1 << _sa;// logic
-		`ALU_OP_SLLV:// shift left logical variable
+      `ALU_OP_SLLV:// shift left logical variable
             alu_reg <= _num2 << _num1[4:0];// logic
-        `ALU_OP_SLT:// set on less than
+      `ALU_OP_SLT:// set on less than
             alu_reg <= (_num1 < _num2) ? 1 : 0;
-        `ALU_OP_SRA:// >>
+      `ALU_OP_SRA:// >>
             alu_reg <= _num2 >>> _sa;// arithmetic	alu_result = ($signed(operandB)) >>> operandA
-        `ALU_OP_SRL:// >>
+      `ALU_OP_SRL:// >>
             alu_reg <= _num2 >> _sa;// logic
-        `ALU_OP_SRLV:// shift right logical variable
+      `ALU_OP_SRLV:// shift right logical variable
             alu_reg <= _num2 << _num1[4:0];// logic
-        `ALU_OP_MINUS:// -
+      `ALU_OP_MINUS:// -
         begin
-            alu_reg <= _num1 - _num2;
-			if((_num1 >= 0) && (_num2 < 0) && (alu_reg < 0))
-			begin
-				overflow <= 1'b1;
-			end
-			else if((_num1 < 0) && (_num2 > 0) && (alu_reg >= 0))
-			begin
-				overflow <= 1'b1;
-			end
-			else
-			begin
-				overflow <= 1'b0;// no overflow
-			end
+        alu_reg <= _num1 - _num2;
+        if ((_num1 >= 0) && (_num2 < 0) && (alu_reg < 0)) begin
+          overflow <= 1'b1;
+        end else if ((_num1 < 0) && (_num2 > 0) && (alu_reg >= 0)) begin
+          overflow <= 1'b1;
+        end else begin
+          overflow <= 1'b0;  // no overflow
         end
-        `ALU_OP_XOR:// xor
+      end
+      `ALU_OP_XOR:// xor
             alu_reg <= _num1 ^ _num2;
-        `ALU_OP_NOR:// nor
+      `ALU_OP_NOR:// nor
             alu_reg <= ~(_num1 | _num2);
 
-        `ALU_OP_DEFAULT:
-            alu_reg <= {_num2[31], _num2};
+      `ALU_OP_DEFAULT: alu_reg <= {_num2[31], _num2};
     endcase
-end
+  end
 
 
 
