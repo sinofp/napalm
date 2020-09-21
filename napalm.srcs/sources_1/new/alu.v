ebc65810537b4d8f199338a7d39affcf998f4a3d
YUPP12138 <70902032+YUPP12138@users.noreply.github.com>
2020-09-22 05:13:17 +0800

    带new的注解是新增的slti，sltiu/////plus更改为无符号运算
    带new的注解是新增的slti，sltiu/////plus更改为无符号运算

--- napalm.srcs/sources_1/new/alu.v
+++ napalm.srcs/sources_1/new/alu.v
@@ -1,86 +1,84 @@
 `timescale 1ns / 1ps
 `include "def.vh"
 
+
 module alu (
-    input [31:0] num1,
-    input [31:0] num2,
-	input [4:0] sa,
-	input [3:0] aluOp,
-    output [31:0] res,
+    input [31:0] _num1,
+    input [31:0] _num2,
+    input [3:0] _alu_op,
+//new	input [31:0] _imm,// for SLTI,SLTIU   传入这俩指令低16位扩展而来的32位的立即数imm是什么
+    output [31:0] alu_res,
     output reg overflow
 );
 
 
-reg[32:0] alu_reg;
-reg[32:0] alu_hi;
-reg[32:0] alu_lo;
-assign res = alu_reg[31:0];
-assign hi = alu_hi[31:0];//for DIV, MULT
-assign lo = alu_lo[31:0];//32 bits
+  reg [32:0] alu_reg;
+  //reg[32:0] alu_hi;
+  //reg[32:0] alu_lo;
+  assign alu_res = alu_reg[31:0];
+  //assign hi = alu_hi[31:0];//for DIV, MULT
+  //assign lo = alu_lo[31:0];//32 bits
 
-always @ (*) begin
-    case (aluOp)
-        `ALU_OP_PLUS:// +
+  wire [4:0] _sa;
+  assign _sa = _num1[4:0];
+  always @(*) begin
+    case (_alu_op)
+      `ALU_OP_PLUS:// +
 		begin
-            alu_reg <= num1 + num2;
-			if((num1 >= 0) && (num2 >= 0) && (alu_reg < 0))
-			begin
-				overflow <= 1'b1;
-			end
-			else if((num1 < 0) && (num2 < 0) && (alu_reg >= 0))
-			begin
-				overflow <= 1'b1;
-			end
-			else
-			begin
-				overflow <= 1'b0;// no overflow
-			end
+        alu_reg <= _num1 + _num2;
+        if (alu_reg < 0)
+		begin
+          overflow <= 1'b1;
 		end
-        `ALU_OP_AND://&
-            alu_reg <= num1 & num2;
-
-	    `ALU_OP_DIV:// / %
+        else
+		begin
+          overflow <= 1'b0;  // no overflow
+        end
+      end
+      `ALU_OP_AND://&
+			alu_reg <= _num1 & _num2;
+      /*	    `ALU_OP_DIV:// / %
 		begin
 			begin
-				alu_hi <= num1 % num2;
+				alu_hi <= _num1 % _num2;
 			end
 			begin
-				alu_lo <= num1 / num2;
+				alu_lo <= _num1 / _num2;
 			end
-				if(num2 == 0)
+				if(_num2 == 0)
 				begin
 					overflow <= 1'b1;
 				end
-				else if((num1 >= 0) && (num2 < 0) && (alu_hi > 0))
+				else if((_num1 >= 0) && (_num2 < 0) && (alu_hi > 0))
 				begin
 					overflow <= 1'b1;
 				end
-				else if((num1 < 0) && (num2 > 0) && (alu_hi >= 0))
+				else if((_num1 < 0) && (_num2 > 0) && (alu_hi >= 0))
 				begin
 					overflow <= 1'b1;
 				end
-				else if((num1 <= 0) && (num2 < 0) && (alu_hi < 0))
+				else if((_num1 <= 0) && (_num2 < 0) && (alu_hi < 0))
 				begin
 					overflow <= 1'b1;
 				end
-				else if((num1 >= 0) && (num2 > 0) && (alu_hi < 0))
+				else if((_num1 >= 0) && (_num2 > 0) && (alu_hi < 0))
 				begin
 					overflow <= 1'b1;
 				end
 				
-				else if((num1 >= 0) && (num2 < 0) && (alu_lo > 0))
+				else if((_num1 >= 0) && (_num2 < 0) && (alu_lo > 0))
 				begin
 					overflow <= 1'b1;
 				end
-				else if((num1 < 0) && (num2 > 0) && (alu_lo >= 0))
+				else if((_num1 < 0) && (_num2 > 0) && (alu_lo >= 0))
 				begin
 					overflow <= 1'b1;
 				end
-				else if((num1 <= 0) && (num2 < 0) && (alu_lo < 0))
+				else if((_num1 <= 0) && (_num2 < 0) && (alu_lo < 0))
 				begin
 					overflow <= 1'b1;
 				end
-				else if((num1 >= 0) && (num2 > 0) && (alu_lo < 0))
+				else if((_num1 >= 0) && (_num2 > 0) && (alu_lo < 0))
 				begin
 					overflow <= 1'b1;
 				end
@@ -90,26 +88,25 @@ always @ (*) begin
 					overflow <= 1'b0;// no overflow
 				end
 		end
-
-        `ALU_OP_MULT://*
+        `ALU_OP_MULT://
         begin
-            {alu_hi[31:0],alu_lo[31:0]} <= num1 * num2;
-            if((num1 >= 0) && (num2 < 0) && ({alu_hi[31:0],alu_lo[31:0]} > 0))
+            {alu_hi[31:0],alu_lo[31:0]} <= _num1 * _num2;
+            if((_num1 >= 0) && (_num2 < 0) && ({alu_hi[31:0],alu_lo[31:0]} > 0))
             begin
 				overflow <= 1'b1;
 			end
 			
-			else if((num1 < 0) && (num2 >= 0) && ({alu_hi[31:0],alu_lo[31:0]} > 0))
+			else if((_num1 < 0) && (_num2 >= 0) && ({alu_hi[31:0],alu_lo[31:0]} > 0))
 			begin
 				overflow <= 1'b1;
 			end
 			
-			else if((num1 <= 0) && (num2 <= 0) && ({alu_hi[31:0],alu_lo[31:0]} < 0))
+			else if((_num1 <= 0) && (_num2 <= 0) && ({alu_hi[31:0],alu_lo[31:0]} < 0))
 			begin
 				overflow <= 1'b1;
 			end
 			
-			else if((num1 >= 0) && (num2 >= 0) && ({alu_hi[31:0],alu_lo[31:0]} < 0))
+			else if((_num1 >= 0) && (_num2 >= 0) && ({alu_hi[31:0],alu_lo[31:0]} < 0))
 			begin
 				overflow <= 1'b1;
 			end
@@ -119,45 +116,83 @@ always @ (*) begin
 				overflow <= 1'b0;// no overflow
 			end
 		end
-        `ALU_OP_OR:// |
-            alu_reg <= num1 | num2;
-        `ALU_OP_SLL:// <<
-            alu_reg <= num1 << sa;// logic
-		`ALU_OP_SLLV:// shift left logical variable
-            alu_reg <= num2 << num1[4:0];// logic
-        `ALU_OP_SLT:// set on less than
-            alu_reg <= (num1 < num2) ? 1 : 0;
-        `ALU_OP_SRA:// >>
-            alu_reg <= num2 >>> sa;// arithmetic	Result = ($signed(operandB)) >>> operandA
-        `ALU_OP_SRL:// >>
-            alu_reg <= num2 >> sa;// logic
-        `ALU_OP_SRLV:// shift right logical variable
-            alu_reg <= num2 << num1[4:0];// logic
-        `ALU_OP_MINUS:// -
-        begin
-            alu_reg <= num1 - num2;
-			if((num1 >= 0) && (num2 < 0) && (alu_reg < 0))
-			begin
-				overflow <= 1'b1;
-			end
-			else if((num1 < 0) && (num2 > 0) && (alu_reg >= 0))
-			begin
-				overflow <= 1'b1;
-			end
-			else
-			begin
-				overflow <= 1'b0;// no overflow
-			end
-        end
-        `ALU_OP_XOR:// xor
-            alu_reg <= num1 ^ num2;
-        `ALU_OP_NOR:// nor
-            alu_reg <= ~(num1 | num2);
+*/
 
-        `ALU_OP_DEFAULT:
-            alu_reg <= {num2[31], num2};
+      `ALU_OP_OR:// |
+            alu_reg <= _num1 | _num2;
+      `ALU_OP_SLL:// <<
+            alu_reg <= _num2 << _sa;// logic
+      `ALU_OP_SLLV:// shift left logical variable
+            alu_reg <= _num2 << _sa;// logic
+      `ALU_OP_SLT:// set on less than
+            alu_reg <= (_num1 < _num2) ? 1 : 0;
+      `ALU_OP_SRA:// >>
+            alu_reg <= _num2 >>> _sa;// arithmetic	alu_result = ($signed(operandB)) >>> operandA
+      `ALU_OP_SRL:// >>
+            alu_reg <= _num2 >> _sa;// logic
+      `ALU_OP_SRLV:// shift right logical variable
+            alu_reg <= _num2 >> _sa;// logic
+      `ALU_OP_MINUS:// -
+        begin
+        alu_reg <= _num1 - _num2;
+        if ((_num1 >= 0) && (_num2 < 0) && (alu_reg < 0)) begin
+          overflow <= 1'b1;
+        end else if ((_num1 < 0) && (_num2 > 0) && (alu_reg >= 0)) begin
+          overflow <= 1'b1;
+        end else begin
+          overflow <= 1'b0;  // no overflow
+        end
+      end
+      `ALU_OP_XOR:// xor
+            alu_reg <= _num1 ^ _num2;
+      `ALU_OP_NOR:// nor
+            alu_reg <= ~(_num1 | _num2);
+
+
+/*new
+	  //add SLTI,SLTIU
+	  //传入的_imm 为 (低16位扩展而来的32位的立即数)
+      `ALU_OP_SLTI:// 有符号比较 rt <- rs < imm   rs中的值与扩展后的立即数imm比较 结果放入rt中
+            begin
+			case({_imm[31],_num1[31]})
+			2'b01:// _imm 为正 _num1 为负
+				begin
+				alu_reg <= 1;
+				end
+			2'b10:// _imm 为负 _num1 为正
+				begin
+				alu_reg <= 0;
+				end
+			2'b00:// _imm,_num1 均为正,比较后31位
+				begin
+					if(_imm[30:0] >= _num1[30:0])
+					begin
+						alu_reg <= 1;
+					end
+					else
+					begin
+						alu_reg <= 0;
+					end
+				end
+			2'b11:// _imm,_num1 均为负,比较后31位
+				begin
+					if(_imm[30:0] >= _num1[30:0])
+					begin
+						alu_reg <= 1;
+					end
+					else
+					begin
+						alu_reg <= 0;
+					end
+				end
+			endcase
+			end
+      `ALU_OP_SLTIU:// 无符号比较 rt <- rs < imm   rs中的值与扩展后的立即数imm比较 结果放入rt中
+            alu_reg <= (_imm > _num1) ? 1 : 0;
+*/
+      `ALU_OP_DEFAULT: alu_reg <= {_num2[31], _num2};
     endcase
-end
+  end
 
 
 
