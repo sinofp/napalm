545c33ec0c3f85bfc626e803a5525db6d1161b7d
sinofp <sinofp@tuta.io>
2020-09-22 12:26:44 +0800

    Add sh, lh support

--- napalm.srcs/sources_1/new/data_mem.v
+++ napalm.srcs/sources_1/new/data_mem.v
@@ -13,13 +13,14 @@ module data_mem (
   reg [7:0] mem[255:0];
 
   assign rd = (_opcode == `LB_OP)? {{24{mem[_addr][7]}}, mem[_addr]}: // load 8 位，符号扩展到 32 位
+              (_opcode == `LH_OP)? {{16{mem[_addr+1][7]}}, mem[_addr+1], mem[_addr]}:
                                    {mem[_addr+3], mem[_addr+2], mem[_addr+1], mem[_addr]};
 
   always @(posedge clk) begin
     if (_we) begin
-      mem[_addr+3] <= _wd[31:24];
-      mem[_addr+2] <= _wd[23:16];
-      mem[_addr+1] <= _wd[15:8];
+      mem[_addr+3] <= (_opcode == `LB_OP)? {8{_wd[7]}}: (_opcode == `LH_OP)? {8{_wd[15]}}: _wd[31:24];
+      mem[_addr+2] <= (_opcode == `LB_OP)? {8{_wd[7]}}: (_opcode == `LH_OP)? {8{_wd[15]}}: _wd[23:16];
+      mem[_addr+1] <= (_opcode == `LB_OP)? {8{_wd[7]}}: _wd[15:8];
       mem[_addr] <= _wd[7:0];
     end
   end
